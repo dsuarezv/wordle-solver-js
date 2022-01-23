1b7e3a8a85f58c0238395c6822aa4b60e291a3c2
David Suárez <dsuarezv@gmail.com>
2022-01-23 11:00:54 +0000

    Reset state to initial conditions after switching language.

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <h1>WORDLE HELPER</h1>
             <div className='Hint'>
                 <p>This little tool will help you find the best solution for the game Wordle by narrowing down the possible words that fit your current tries.</p>
                 <p>You can't play the game here, but you can get the list of words that fit in your Wordle game.</p>
